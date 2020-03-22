70944940ed18f7241fb0ef97040bb590963340b5
luca <lucat1@users.noreply.github.com>
2020-03-22 19:29:59 +0100

    add more dev scripts

--- scripts/build.ts
+++ scripts/build.ts
@@ -4,7 +4,7 @@ import { spawn } from './spawn'
 async function main() {
   const pkgs = await getPackages()
   for(const pkg of pkgs) {
-    spawn('dev', pkg)
+    await spawn('build', pkg)
   }
 }
 
---
+++ scripts/spawn.ts
@@ -0,0 +1,24 @@
+import { spawn as childSpawn } from 'child_process'
+import { Package } from '@manypkg/get-packages'
+
+export async function spawn(script: string, pkg: Package) {
+  const child = childSpawn('yarn', [script], { cwd: pkg.dir })
+
+  for(const pipe of ['stdout', 'stderr']) {
+    // whether the previous data ended with a \n
+    let nl = true
+    child[pipe].on('data', (_data: Buffer) => {
+      const data = _data.toString()
+      if(nl) {
+        process[pipe].write(`${pkg.packageJson.name} ~ ${data}`)
+        nl = false
+      } else {
+        process[pipe].write(data)
+        if(data.endsWith('\n')) {
+          nl = true
+        }
+      }
+    })
+  }
+  process.stdin.pipe(child.stdin)
+}
