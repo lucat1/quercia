1c9afbfdcaf901602e41d73958158e43667c6508
luca <lucat1@users.noreply.github.com>
2020-03-25 19:21:48 +0100

    fix indentation

--- template.go
+++ template.go
@@ -9,14 +9,14 @@ const defaultTemplate = `
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-	<div id="__quercia">__INSERT_QUERCIA_PRERENDER__</div>
+  <div id="__quercia">__INSERT_QUERCIA_PRERENDER__</div>
   <script id="__QUERCIA_DATA__" type="application/json" crossorigin="anonymous">
     __INSERT_QUERCIA_DATA__
-	</script>
-	<script>__INSERT_QUERCIA_WEBPACK_RUNTIME__</script>
-	<script src="/__quercia/__INSERT_QUERCIA_VENDOR__"></script>
-	<script src="/__quercia/__INSERT_QUERCIA_PAGE__"></script>
-	<script src="/__quercia/__INSERT_QUERCIA_RUNTIME__"></script>
+  </script>
+  <script>__INSERT_QUERCIA_WEBPACK_RUNTIME__</script>
+  <script src="/__quercia/__INSERT_QUERCIA_VENDOR__"></script>
+  <script src="/__quercia/__INSERT_QUERCIA_PAGE__"></script>
+  <script src="/__quercia/__INSERT_QUERCIA_RUNTIME__"></script>
 </body>
 </html>
 `
