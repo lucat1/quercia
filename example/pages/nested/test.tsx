52172d7f7aba10b2cf704fa537bd418a222bc616
luca <lucat1@users.noreply.github.com>
2020-04-07 20:16:51 +0200

    :fire: removed example and regenerated yarn.lock

--- example/pages/nested/test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import { Link } from '@quercia/quercia'
-
-export default () => (
-  <div>
-    <Link to='/'>amazing link</Link>
-    <Link to='/test'>test</Link>
-  </div>
-)
